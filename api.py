40ae51573d0a9f3490e23234c74b9b7a87f2ac36
Jason Knight <Jason@JasonKnight.us>
2014-08-30 12:20:12 -0500

    Better wunderground source

--- api.py
+++ api.py
@@ -5,18 +5,24 @@ import urllib2
 import json
 
 key = open('wgkey.txt').read().strip()
-url = "http://api.wunderground.com/api/{}/conditions/q/77845.json".format(key)
+#url = "http://api.wunderground.com/api/{}/conditions/q/77845.json".format(key)
+url = "http://api.wunderground.com/api/{}/conditions/q/pws:KTXCOLLE20.json".format(key)
+
+def grab_and_record(url):
+    try:
+        resp = urllib2.urlopen(url).read()
+    except:
+        return
+    data = [{'points':[[resp]],
+            'name':'wunderground',
+            'columns':['data']}]
+    db.write_points(data)
 
 if __name__=='__main__':
     db = influxdb.InfluxDBClient(host='raspberrypi', database='hvac')
     while True:
-        resp = urllib2.urlopen(url).read()
-        #respdata = json.loads(resp.read())
-        data = [{'points':[[resp]],
-                'name':'wunderground',
-                'columns':['data']}]
-        db.write_points(data)
+        grab_and_record(url)
         print("Wunderground data written, waiting 30 minutes...")
-        time.sleep(60*30)
+        time.sleep(60*10)
 
 
